47df04d25e469da30bbcf539cb1f8715308697cd
Piyush Kumar Agarwal <chatwithapiyush@gmail.com>
2024-11-03 01:17:09 +0530

    Second commit

---
+++ routes/dashboard.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>Dashboard</title>
+    </head>
+    <body>
+        <button>Back</button>
+        <button>Logout</button>
+        <h1>Online Voting System</h1>
+        <hr>
+
+        <div id="profile">
+        <div id="group">
+        </div>
+        </div>
+    </body>
+
+</html>
